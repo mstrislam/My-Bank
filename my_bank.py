53aa677ee528c3ca9e338dfe55185d05d021179c
mstrislam <audaserieva4@gmail.com>
2024-11-01 18:46:37 +0600

    eeee

---
+++ my_bank.py
@@ -0,0 +1,5 @@
+num1=int(input("введите первое число"))
+num2=int(input("введите второе число"))
+num3=int(input("введите третье число"))
+smolled=min(num1,num2,num3)
+print("найменьшее число ",smolled)
